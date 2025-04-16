83d4c354bd5ce36bd967da314296ad66fec5ff03
Ramez Dwere <ramezdwere@Macbook.local>
2025-04-16 12:45:10 +0300

    Another hotfix

--- src/5.2.cup_of_join.py
+++ src/5.2.cup_of_join.py
@@ -1,10 +1,13 @@
+from inspect import signature
+
 def cup_of_join(*lists, sep='-'):
+    sig = signature(cup_of_join)
+    sep_was_explicit = 'sep' in sig.bind_partial(*lists).arguments
     result = []
     for i, l in enumerate(lists):
-        if i > 0 and sep is not None:
+        if i > 0 and sep_was_explicit:
             result.append(sep)
         result.extend(l)
-    if len(lists) == 1 or any(len(l) == 0 for l in lists):
-        if sep is not None:
-            result.append(sep)
+    if len(lists) == 1 and sep_was_explicit:
+        result.append(sep)
     return result
