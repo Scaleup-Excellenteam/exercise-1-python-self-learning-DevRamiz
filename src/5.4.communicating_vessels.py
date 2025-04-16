add06cffe66596970a55112e76e8fc2518d553bd
Ramez Dwere <ramezdwere@Macbook.local>
2025-04-16 12:05:16 +0300

    Fix: add interleave function for test compatibility

--- src/5.4.communicating_vessels.py
+++ src/5.4.communicating_vessels.py
@@ -4,3 +4,6 @@ def generator_interleave(a: list, b: list) -> Generator:
     for x, y in zip(a, b):
         yield x
         yield y
+
+def interleave(a: list, b: list) -> list:
+    return list(generator_interleave(a, b))
