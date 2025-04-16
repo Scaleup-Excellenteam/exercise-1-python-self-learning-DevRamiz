b0d81e748f29acd20ed4f44b3c06f9818b2acb5e
Ramez Dwere <ramezdwere@Macbook.local>
2025-04-16 12:15:36 +0300

    Fix: add generator_interleave and interleave

--- src/5.4.communicating_vessels.py
+++ src/5.4.communicating_vessels.py
@@ -1,4 +1,9 @@
-def interleave(a, b):
+from typing import Generator
+
+def generator_interleave(a: list, b: list) -> Generator:
     for x, y in zip(a, b):
         yield x
         yield y
+
+def interleave(a: list, b: list) -> list:
+    return list(generator_interleave(a, b))
