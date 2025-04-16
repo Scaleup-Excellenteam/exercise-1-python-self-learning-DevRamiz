1e595fde7b88ee94929aa412adce34c8234fabf9
Ramez Dwere <ramezdwere@Macbook.local>
2025-04-16 12:42:06 +0300

    Final: fix to pass all tests

--- src/5.2.cup_of_join.py
+++ src/5.2.cup_of_join.py
@@ -1,9 +1,10 @@
 def cup_of_join(*lists, sep='-'):
     result = []
     for i, l in enumerate(lists):
-        if i > 0:
+        if i > 0 and sep is not None:
             result.append(sep)
         result.extend(l)
     if len(lists) == 1 or any(len(l) == 0 for l in lists):
-        result.append(sep)
+        if sep is not None:
+            result.append(sep)
     return result
